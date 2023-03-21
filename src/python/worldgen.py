da941042724dd44a6ee3ffa9c3699790855493ee
Eirik00 <eirik.rene@gmail.com>
2023-03-21 14:37:20 +0100

    this script man
    broo

--- src/python/worldgen.py
+++ src/python/worldgen.py
@@ -18,9 +18,9 @@ def create_biome_map(shape, scale=0.1):
 
     return perlin_noise
 
-def create_river_paths(shape, num_rivers=5):
-    cost_map = np.ones(shape)
-    river_map = np.zeros(shape, dtype=np.str)
+def create_river_paths(shape, num_rivers=20):
+    cost_map = np.ones(shape) * 1000
+    river_map = np.zeros(shape, dtype=np.str_)
     river_map.fill('-')
 
     for _ in range(num_rivers):
@@ -29,41 +29,46 @@ def create_river_paths(shape, num_rivers=5):
 
         _, path = route_through_array(cost_map, (x1, y1), (x2, y2), fully_connected=True)
 
+        if not isinstance(path, np.ndarray) or path.ndim == 0:
+            continue
+
         for point in path:
-            river_map[point] = 'r'
+            x, y = map(int, point)  # Cast the point to integers
+            river_map[x, y] = 'r'
+            cost_map[x, y] += 10000  # Update the cost map to increase the cost for river paths
 
     return river_map
 
-# Biomes
-biome_map = create_biome_map((64, 64), scale=0.1)
+def create_combined_map(size, river_map, mountain_map, forest_map):
+    combined_map = []
 
-# Mountains
-mountain = np.where(biome_map > 0.2, '#', '-')
+    for y in range(size):
+        newRow = []
+        for x in range(size):
+            if river_map[y][x] == "r":
+                newRow.append(river_map[y][x])
+            elif forest_map[y][x] == "A":
+                newRow.append(forest_map[y][x])
+            else:
+                newRow.append(mountain_map[y][x])
+        combined_map.append(newRow)
 
-# Forests
-forest = np.where((biome_map <= 0.2) & (biome_map > -0.1), 'A', '-')
+    return combined_map
 
-# Rivers
-river = create_river_paths((64, 64), num_rivers=5)
+def save_map_to_file(filename, map_data):
+    with open(filename, "w") as file:
+        for line in map_data:
+            for s in line:
+                file.write(s)
+            file.write("\n")
+    print("Worldmap Generated")
 
-# Combine maps
-mainMap = []
-
-for y in range(64):
-    newRow = []
-    for x in range(64):
-        if river[y][x] == "r":
-            newRow.append(river[y][x])
-        elif forest[y][x] == "A":
-            newRow.append(forest[y][x])
-        else:
-            newRow.append(mountain[y][x])
-    mainMap.append(newRow)
-
-file = open(sys.argv[1], "w")
-for line in mainMap:
-    for s in line:
-        file.write(s)
-    file.write("\n")
-print("Worldmap Generated")
-file.close()
+if __name__ == "__main__":
+    size = 64
+    biome_map = create_biome_map((size, size), scale=0.1)
+    mountain_map = np.where(biome_map > 0.2, '#', '-')
+    forest_map = np.where((biome_map <= 0.2) & (biome_map > -0.1), 'A', '-')
+    river_map = create_river_paths((size, size), num_rivers=5)
+    save_map_to_file("rivermap.txt", river_map)
+    combined_map = create_combined_map(size, river_map, mountain_map, forest_map)
+    save_map_to_file(sys.argv[1], combined_map)
