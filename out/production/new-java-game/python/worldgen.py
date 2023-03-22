646cc0ce36b0c0e678cf02cc5b5e461a6192890b
Eirik00 <eirik.rene@gmail.com>
2023-03-22 01:03:55 +0100

    improved worldgen
    Added river generation

--- out/production/new-java-game/python/worldgen.py
+++ out/production/new-java-game/python/worldgen.py
@@ -1,27 +1,23 @@
 import numpy as np
 import sys
 from scipy.ndimage import zoom
-from noise import pnoise2
 from skimage.graph import route_through_array
 from random import randint
 
 np.set_printoptions(threshold=sys.maxsize)
 
-def create_biome_map(shape, scale=0.1):
-    perlin_noise = np.zeros(shape)
 
-    for i in range(shape[0]):
-        for j in range(shape[1]):
-            x = i * scale
-            y = j * scale
-            perlin_noise[i, j] = pnoise2(x, y)
-
-    return perlin_noise
-
-def create_river_paths(shape, num_rivers=5):
-    cost_map = np.ones(shape)
-    river_map = np.zeros(shape, dtype=np.str)
+def create_river_paths(shape, num_rivers=5, thickness=1):
+    cost_map = np.ones(shape) * 1000
+    river_map = np.zeros(shape, dtype=np.str_)
     river_map.fill('-')
+
+    def add_river(x, y):
+        for dx in range(-thickness, thickness + 1):
+            for dy in range(-thickness, thickness + 1):
+                if 0 <= x + dx < shape[0] and 0 <= y + dy < shape[1]:
+                    river_map[x + dx, y + dy] = 'r'
+
 
     for _ in range(num_rivers):
         x1, y1 = randint(0, shape[0] - 1), randint(0, shape[1] - 1)
@@ -29,36 +25,52 @@ def create_river_paths(shape, num_rivers=5):
 
         _, path = route_through_array(cost_map, (x1, y1), (x2, y2), fully_connected=True)
 
-        for point in path:
-            river_map[point] = 'r'
+        if not isinstance(_, list):
+            continue
+
+        for point in _:
+            x, y = map(int, point)  # Cast the point to integers
+            add_river(x, y)
+            cost_map[x, y] += 10000  # Update the cost map to increase the cost for river paths
 
     return river_map
 
-# Biomes
-biome_map = create_biome_map((64, 64), scale=0.1)
 
-# Mountains
-mountain = np.where(biome_map > 0.2, '#', '-')
+mountain = np.random.uniform(size=(4, 4))
+mountain = zoom(mountain, 16)
+mountain = mountain > 0.3
+mountain = np.where(mountain, '-', '#')
+mountain = np.array(mountain)
 
-# Forests
-forest = np.where((biome_map <= 0.2) & (biome_map > -0.1), 'A', '-')
+forest = np.random.uniform(size=(16, 16))
+forest = zoom(forest, 4)
+forest = forest > 0.5
+forest = np.where(forest, '-', 'A')
+forest = np.array(forest)
 
-# Rivers
-river = create_river_paths((64, 64), num_rivers=5)
+river_map = create_river_paths((64, 64), num_rivers=5, thickness=1)
 
-# Combine maps
 mainMap = []
 
-for y in range(64):
+y = 0
+for row in forest:
+    x = 0
     newRow = []
-    for x in range(64):
-        if river[y][x] == "r":
-            newRow.append(river[y][x])
-        elif forest[y][x] == "A":
-            newRow.append(forest[y][x])
+    for obj in row:
+        if river_map[y][x] == "r":
+            newRow.append("r")
+        elif obj == "A":
+            if mountain[y][x] == "-":
+                newRow.append("A")
+            else:
+                newRow.append(mountain[y][x])
         else:
             newRow.append(mountain[y][x])
+        x += 1
+    y += 1
     mainMap.append(newRow)
+
+mainMap = mainMap[:64]
 
 file = open(sys.argv[1], "w")
 for line in mainMap:
